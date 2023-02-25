31e593de18cc40cbeff50db465e06162a7d505b3
timothylth93@gmail.com <timothylth93@gmail.com>
2023-02-25 02:46:19 +0000

    updating fct_orders.sql

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,16 +1,30 @@
-with payments as (
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
 
+payments as (
     select * from {{ ref('stg_payments') }}
 ),
 
-final as (
-    select 
+order_payments as (
+    select
         order_id,
-        customer_id,
-        amount
-        
+        sum(case when status = 'success' then amount end) as amount
+
     from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select *
-from final 
+select * from final
--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -1,8 +1,10 @@
 select 
-    id as customer_id,
+    id as payment_id,
     orderid as order_id,
+    paymentmethod as payment_method,
     status,
-    amount,
-    created
-    
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
 from stripe.payment
